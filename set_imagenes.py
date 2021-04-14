09110159cb51069c377b67ead6aa90c18fea67f9
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-04-14 17:50:42 -0500

    archivo1

---
+++ set_imagenes.py
@@ -0,0 +1,12 @@
+import os
+import threading
+
+#entrenamiento no 96
+#entrenamiento yes 155
+#test no 98
+#test yes 155
+directorio_raiz = os.listdir('../') #nombre carpeta root
+sub_directoris_raiz = os.listdir('./') #archivos dentro de root
+
+
+

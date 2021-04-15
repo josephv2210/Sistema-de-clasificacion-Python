698f722596b58ec195780e69555dd86fbce1afac
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-04-14 20:18:14 -0500

    totalimgs

--- set_imagenes.py
+++ set_imagenes.py
@@ -1,16 +1,12 @@
 import os
 
-#entrenamiento no 96
-#entrenamiento yes 155
-#test no 98
-#test yes 155
-
 def busca_directorios(num, pat):                
     for f in os.listdir(pat): 
         if os.path.isdir(pat+'/'+f):
             num.append(pat+'/'+f)
             busca_directorios(num, pat+'/'+f)        
     return num
+
 if __name__ == '__main__':
     #nombre carpeta root
     #buscamos dentro del archivo root la carpeta del proyecto
@@ -23,6 +19,22 @@ if __name__ == '__main__':
 
     #accedemos a la carpeta de las imagenes
     sub_directorios_imagenes = busca_directorios([], './imagenes')
-    print(sub_directorios_imagenes)
     print(f"subdirectorios en carpeta de imagenes: {len(sub_directorios_imagenes)} ")
+
+    #buscamos cuantas imagenes tiene cada directorio y almacenamos en un diccionario
+    imagenes_dict = {}
+    temp = []
+    for i in sub_directorios_imagenes:
+        for f in os.listdir(i): 
+            if os.path.isfile(i+'/'+f):
+                temp.append(i+'/'+f)    
+        imagenes_dict[i] = temp
+        temp = []
+    
+    #total de imagenes
+    suma = 0
+    for i in imagenes_dict:
+        print(f"{i} : {len(imagenes_dict[i])} imagenes")
+        suma += len(imagenes_dict[i])
+    print(f"TOTAL IMAGENES: {suma}")
 

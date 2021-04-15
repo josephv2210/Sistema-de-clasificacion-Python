686c928e1e77d483687dd39be8082de3ca70c250
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-04-14 19:32:39 -0500

    subdirectorios

--- set_imagenes.py
+++ set_imagenes.py
@@ -1,12 +1,28 @@
 import os
-import threading
 
 #entrenamiento no 96
 #entrenamiento yes 155
 #test no 98
 #test yes 155
-directorio_raiz = os.listdir('../') #nombre carpeta root
-sub_directoris_raiz = os.listdir('./') #archivos dentro de root
 
+def busca_directorios(num, pat):                
+    for f in os.listdir(pat): 
+        if os.path.isdir(pat+'/'+f):
+            num.append(pat+'/'+f)
+            busca_directorios(num, pat+'/'+f)        
+    return num
+if __name__ == '__main__':
+    #nombre carpeta root
+    #buscamos dentro del archivo root la carpeta del proyecto
+    for f in os.listdir('../'):
+        if f.lower() == 'sistema-de-clasificacion-python':
+            directorio_raiz = f
 
+    #archivos dentro de la carpeta
+    sub_directorios_raiz = os.listdir('./') 
+
+    #accedemos a la carpeta de las imagenes
+    sub_directorios_imagenes = busca_directorios([], './imagenes')
+    print(sub_directorios_imagenes)
+    print(f"subdirectorios en carpeta de imagenes: {len(sub_directorios_imagenes)} ")
 
